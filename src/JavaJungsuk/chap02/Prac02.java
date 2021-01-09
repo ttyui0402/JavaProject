e4f88635ae4643f95050aec71f211d8886426e56
ttyui0402 <ttyui0402@gmail.com>
2021-01-09 13:43:25 +0900

    Initial commit

---
+++ src/JavaJungsuk/chap02/Prac02.java
@@ -0,0 +1,8 @@
+package JavaJungsuk.chap02;
+
+public class Prac02 {
+    public static void main(String[] args) {
+        long regNo = 9104021111111L;
+        System.out.println(regNo);
+    }
+}
---
+++ src/JavaJungsuk/chap02/Prac05.java
@@ -0,0 +1,15 @@
+package JavaJungsuk.chap02;
+
+public class Prac05 {
+    public static void main(String[] args) {
+        System.out.println("1" + "2");
+        System.out.println(true + "");
+
+        System.out.println('A' + 'B');
+        System.out.println('1' + 2);
+        System.out.println('1' + '2');
+        System.out.println('J' + "ava");
+//        System.out.println(true + null);
+
+    }
+}
